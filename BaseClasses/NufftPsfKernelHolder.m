7b9a5e47806887ef99e3af3e2454e1464ca8a744
rstobbe <rstobbe@ualberta.ca>
2024-03-05 12:22:45 -0700

    update

---
+++ BaseClasses/NufftPsfKernelHolder.m
@@ -0,0 +1,84 @@
+ %==================================================================
+% 
+%==================================================================
+
+classdef NufftPsfKernelHolder < handle
+
+properties (SetAccess = private)                   
+    KernelFile = 'KBCw2b16ss4'                   
+    Kernel
+    InvFiltFile
+    InvFilt
+end
+
+methods 
+   
+%==================================================================
+% Constructor
+%==================================================================  
+function obj = NufftPsfKernelHolder             
+end 
+
+%==================================================================
+% SetKernelFile
+%==================================================================   
+        function SetKernelFile(obj,val)
+            obj.KernelFile = val;
+        end          
+
+%==================================================================
+% SetInvFiltFile
+%==================================================================   
+        function SetInvFiltFile(obj,val)
+            obj.InvFiltFile = val;
+            load([obj.StitchSupportingPath,'InverseFilters',filesep,'IF_',obj.InvFiltFile,'.mat']);              
+            obj.InvFilt = saveData.IFprms;
+        end                 
+        
+%==================================================================
+% Initialize
+%==================================================================   
+        function Initialize(obj,AcqInfo,StitchIt)   
+            
+            %------------------------------------------------------
+            % Load Kernel
+            %------------------------------------------------------
+            if isempty(StitchIt.StitchSupportingPath)
+                loc = mfilename('fullpath');
+                ind = strfind(loc,'StitchIt');
+                StitchIt.SetStitchSupportingPath([loc(1:ind-1),'StitchSupporting',filesep]); 
+            end
+            load([StitchIt.StitchSupportingPath,'Kernels',filesep,'Kern_',obj.KernelFile,'.mat']);
+            obj.Kernel = saveData.KRNprms;
+
+            %------------------------------------------------------
+            % Test/Load InvFilt
+            %------------------------------------------------------            
+            Matrix = AcqInfo.Fov/AcqInfo.Vox;
+            SubSamp = obj.Kernel.DesforSS;
+            SubSampMatrix = SubSamp * Matrix + 4;
+            PossibleZeroFill = obj.Kernel.PossibleZeroFill;
+            if ~isempty(StitchIt.BaseMatrix)
+                StitchIt.SetGridMatrix(StitchIt.BaseMatrix*SubSamp);
+            end
+            if isempty(StitchIt.GridMatrix)
+                ind = find(PossibleZeroFill > SubSampMatrix,1,'first');
+                StitchIt.SetGridMatrix(PossibleZeroFill(ind));
+            end
+            if StitchIt.GridMatrix < SubSampMatrix
+                error(['Specified GridMatrix is too small. Min: ',num2str(round(SubSampMatrix))]);
+            end
+            if isempty(obj.InvFiltFile)
+                if StitchIt.TestFov2ReturnGridMatrix
+                    obj.InvFiltFile = [obj.KernelFile,'zf',num2str(StitchIt.GridMatrix),'S'];
+                else
+                    obj.InvFiltFile = [obj.KernelFile,'zf',num2str(StitchIt.GridMatrix),'SB'];
+                end
+                load([StitchIt.StitchSupportingPath,'InverseFilters',filesep,'IF_',obj.InvFiltFile,'.mat']);              
+                obj.InvFilt = saveData.IFprms;
+            end
+            StitchIt.SetBaseMatrix(StitchIt.GridMatrix/SubSamp);
+            
+        end            
+    end
+end
---
+++ _Reconstruction/CreatePsfV1a.m
@@ -0,0 +1,94 @@
+%==================================================================
+% 
+%  
+%==================================================================
+
+classdef CreatePsfV1a < handle
+
+properties (SetAccess = private)                   
+    Method = 'CreatePsfV1a'
+    BaseMatrix
+    AcqInfo
+    ResetGpus = 1
+    DispStatObj
+end
+
+methods 
+   
+%==================================================================
+% Constructor
+%==================================================================  
+function ReconObj = CreatePsfV1a()              
+    ReconObj.DispStatObj = DisplayStatusObject();
+end
+
+%==================================================================
+% CreateImage
+%==================================================================  
+function [Image,err] = CreateImage(ReconObj)         
+    %% Reset GPUs
+    if ReconObj.ResetGpus
+        ReconObj.DispStatObj.Status('Reset GPUs',2);
+        for n = 1:gpuDeviceCount
+            gpuDevice(n);
+        end
+    end
+
+    %% Create Psf
+    ReconObj.DispStatObj.Status('Create Psf',2);
+    StitchItPsf = StitchItNufftPsfV1a();                                          
+    StitchItPsf.SetBaseMatrix(ReconObj.BaseMatrix);
+    StitchItPsf.SetSubSample(2.5);
+    StitchItPsf.SetFov2ReturnGridMatrix;
+    RxChannels = 1;
+    StitchItPsf.Initialize(ReconObj.AcqInfo{1},RxChannels); 
+    Data = ones([ReconObj.AcqInfo{1}.NumTraj,ReconObj.AcqInfo{1}.NumCol],'like',single(1+1i)); 
+    Image = StitchItPsf.CreateImage(Data);
+
+end
+
+%==================================================================
+% Set
+%==================================================================  
+%% Set
+function SetBaseMatrix(ReconObj,val)    
+    ReconObj.BaseMatrix = val;
+end
+function SetAcqInfo(ReconObj,val)    
+    ReconObj.AcqInfo = val;
+end
+function SetAcqInfoRxp(ReconObj,val)    
+    ReconObj.AcqInfoRxp = val;
+end
+function SetAcqInfoOffRes(ReconObj,val)    
+    ReconObj.AcqInfoOffRes = val;
+end
+function SetReconNumber(ReconObj,val)    
+    ReconObj.ReconNumber = val;
+end
+function SetRcvrs(ReconObj,val)    
+    ReconObj.Rcvrs = val;
+end
+function SetOffResMap(ReconObj,val)    
+    ReconObj.OffResMap = val;
+end
+function SetShift(ReconObj,val)    
+    ReconObj.Shift = val;
+    ReconObj.UseExternalShift = 1;
+end
+function SetOffResCorrection(ReconObj,val)    
+    ReconObj.OffResCorrection = val;
+end
+function SetUseExternalShift(ReconObj,val)    
+    ReconObj.UseExternalShift = val;
+end
+function SetDisplayRxProfs(ReconObj,val)    
+    ReconObj.DispStatObj.SetDisplayRxProfs(val);
+end
+function SetDisplayOffResMap(ReconObj,val)    
+    ReconObj.DispStatObj.SetDisplayOffResMap(val);
+end
+
+
+end
+end
