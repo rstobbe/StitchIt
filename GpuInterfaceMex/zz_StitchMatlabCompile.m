e5bd7501edf56d77b4a4e615c92cf38d443c4fe5
Rob Stobbe <50251379+rstobbe@users.noreply.github.com>
2024-07-25 13:24:58 -0600

    update

--- GpuInterfaceMex/zz_StitchMatlabCompile.m
+++ GpuInterfaceMex/zz_StitchMatlabCompile.m
@@ -1,10 +1,10 @@
 function zz_StitchMatlabCompile
 
-CC = '86';
+CC = '75';
 Lib = ['CUDA',CC,'_Library230823_113.lib'];
 
 %CUDApath = getenv('CUDA_PATH_V11_3'); 
-CUDApath = getenv('CUDA_PATH_V12_5');   
+CUDApath = getenv('CUDA_PATH_V12_4');   
 CUDApath = [CUDApath,'\lib\x64'];
 CUDAlib = cd;
 
